97d13d12c39ccdc545ecddbaa228e7c857fe379b
Dan Lindsay <dlindsay72@hotmail.com>
2016-12-19 14:41:05 -0500

    Basic setup done, label and button outlets and actions created

--- JustFacts/MainVC.swift
+++ JustFacts/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  JustFacts
 //
 //  Created by Dan Lindsay on 2016-12-19.
@@ -8,7 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
+    
+    
+    @IBOutlet weak var factLabel: UILabel!
+    @IBOutlet weak var factBtn: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showFactPressed(_ sender: UIButton) {
+        
+    }
 
 }
 
